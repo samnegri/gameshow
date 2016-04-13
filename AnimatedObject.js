d18bb45c9c135e9243d0e6136b697de28f46b507
Matheus <matheusmr13@hotmail.com>
2016-04-12 21:16:28 -0300

    hue

--- AnimatedObject.js
+++ AnimatedObject.js
@@ -1,15 +1,15 @@
-(function($) {
-    var AnimatedObject = function() {
-        var animatedObject = {
-            spriteImg : image,
-            
-            draw : function(ctx) {
-                console.info(image);
-                ctx.drawImage(image,0,0,53,67,0,0,53,67); 
-            }
-            
-        }
-        return animatedObject;
-    };
-    this.AnimatedObject = AnimatedObject;
-}).apply(this,[$]);
+(function ($) {
+	var AnimatedObject = function () {
+		var animatedObject = {
+			spriteImg: image,
+
+			draw: function (ctx) {
+				console.info(image);
+				ctx.drawImage(image, 0, 0, 53, 67, 0, 0, 53, 67);
+			}
+
+		}
+		return animatedObject;
+	};
+	this.AnimatedObject = AnimatedObject;
+}).apply(this, [$]);
--- calc.js
+++ calc.js
@@ -72,7 +72,7 @@ function toRadians(angle) {
 };
 
 $('.launch').click(function () {
-	var a = Ball.initialize();
+	var a = new Ball();
 	a.angle = $('.angle').val();
 	a.velocity = $('.velocity').val();
 	console.info('angle: ', a.angle, '   velocity: ', a.velocity);
